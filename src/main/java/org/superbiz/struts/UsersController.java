fd37cfe3b8566cbfa1b233b8bc48687965b9f33e
Shrinath Venkatsubramaniam <shrinath.venkatsubramaniam@cognizant.com>
2017-12-18 11:13:39 -0700

    Updated with spring modernization

---
+++ src/main/java/org/superbiz/struts/UsersController.java
@@ -0,0 +1,52 @@
+package org.superbiz.struts;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class UsersController {
+
+    private final UserRepository userRepository;
+
+    public UsersController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/addUser")
+    public String addUserForm() {
+        return "addUserForm";
+    }
+
+    @PostMapping("/addUser")
+    public String addedUser(User user) {
+        userRepository.save(user);
+        return "addedUser";
+    }
+
+    @GetMapping("/findUser")
+    public String findUserForm() {
+        return "findUserForm";
+    }
+
+    @PostMapping("/findUser")
+    public String findUser(@RequestParam long id, Model model) {
+        User user = userRepository.findOne(id);
+
+        if (user == null) {
+            model.addAttribute("errorMessage", "User not found");
+            return "findUserForm";
+        }
+
+        model.addAttribute("user", user);
+        return "displayUser";
+    }
+
+    @GetMapping("/list")
+    public String listUsers(Model model) {
+        model.addAttribute("users", userRepository.findAll());
+        return "displayUsers";
+    }
+}
